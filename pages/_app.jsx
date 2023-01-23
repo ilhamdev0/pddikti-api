6cf73e5108870bb3a1f0f445d92f338f3a60a642
ilhamdev0 <57636145+ilhamdev0@users.noreply.github.com>
2023-01-24 00:56:55 +0700

    add author

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,6 @@
 import 'styles/global.css'
 
 export default function MyApp({ Component, pageProps }) {
+    console.log("author: https://ilhamdev.pages.dev");
     return <Component {...pageProps} />
 }
