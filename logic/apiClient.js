cd513c58b2d907ca869da0ef019aed7d4fbf8f5b
ilhamdev0 <57636145+ilhamdev0@users.noreply.github.com>
2023-01-26 14:21:52 +0700

    url payload encryption

--- logic/apiClient.js
+++ logic/apiClient.js
@@ -1,22 +1,33 @@
 import { axiosClient, APITimeout, internalAPI } from './apiFactory';
+import Cryptr from 'cryptr';
+
+const encrypt = (data) => {
+    return new Cryptr(
+        process.env.NEXT_PUBLIC_API_SECRET,
+        {
+            pbkdf2Iterations: parseInt(process.env.NEXT_PUBLIC_API_PBKDF2ITERATION),
+            saltLength: parseInt(process.env.NEXT_PUBLIC_API_SALTLENGTH)
+        }
+    ).encrypt(data)
+}
 
 const payload_creator = (nama, url) => {
     return {
         "method": "get",
         "timeout": APITimeout,
-        "url": decodeURI(`${url}${nama}`)
+        "url": encrypt(decodeURI(`${url}${nama}`))
     }
 }
 
 export const getMahasiswa = (nama) => {
-    const url = 'https://api-frontend.kemdikbud.go.id/hit_mhs/'
+    const url = process.env.NEXT_PUBLIC_URL_MHS
     const data = payload_creator(nama, url)
-    
+
     return axiosClient.post(internalAPI, data);
 }
 
 export const getDosen = (nama) => {
-    const url = 'https://api-frontend.kemdikbud.go.id/hit/'
+    const url = process.env.NEXT_PUBLIC_URL_DOSEN
     const data = payload_creator(nama, url)
 
     return axiosClient.post(internalAPI, data);
--- pages/api/server.jsx
+++ pages/api/server.jsx
@@ -1,7 +1,20 @@
 import { axiosServer } from 'logic/apiFactory';
+import Cryptr from 'cryptr';
+
+const decrypt = (data) => {
+    return new Cryptr(
+        process.env.NEXT_PUBLIC_API_SECRET,
+        {
+            pbkdf2Iterations: parseInt(process.env.NEXT_PUBLIC_API_PBKDF2ITERATION),
+            saltLength: parseInt(process.env.NEXT_PUBLIC_API_SALTLENGTH)
+        }
+    ).decrypt(data)
+}
 
 export default function handler(req, res) {
     if (req.method === 'POST') {
+        req.body.url = decrypt(req.body.url)
+
         axiosServer(req.body)
             .then(r => res.status(200).json(r.data))
             .catch(err => res.status(400).json(err))
